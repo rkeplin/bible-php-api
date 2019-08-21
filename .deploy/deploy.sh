32a3867db3994a0eacd7648e99fd18eb50b6094d
Rob <rkeplin@gmail.com>
2019-08-21 15:19:35 -0400

    Setup deployment script

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -3,7 +3,12 @@ openssl aes-256-cbc -K $encrypted_42099b4af021_key -iv $encrypted_42099b4af021_i
 
 chmod 0400 $(pwd)/.deploy/travis_id_rsa
 
+<<<<<<< HEAD
 ssh -t -i $(pwd)/.deploy/travis_id_rsa travis@jersey1.rkeplin.com 'sed -i "s/image: rkeplin\/bible-php-api:[a-zA-Z0-9]*/image: rkeplin\/bible-php-api:'"$TRAVIS_BUILD_NUMBER"'/g" /opt/stacks/bible.yml'
 ssh -t -i $(pwd)/.deploy/travis_id_rsa travis@jersey1.rkeplin.com 'docker stack deploy -c /opt/stacks/bible.yml bible'
+=======
+ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey1.rkeplin.com 'sed -i "s/image: rkeplin\/bible-php-api:[a-zA-Z0-9]*/image: rkeplin\/bible-php-api:'"$TRAVIS_BUILD_NUMBER"'/g" /opt/stacks/bible.yml'
+ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey1.rkeplin.com 'docker stack deploy -c /opt/stacks/bible.yml bible'
+>>>>>>> Setup deployment script
 
 rm -rf $(pwd)/.deploy/travis_id_rsa
