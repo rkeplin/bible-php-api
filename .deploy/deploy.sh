d661c0f51de11c3730b5bfbd5bde1b0ef6bf5f7c
Rob <rkeplin@gmail.com>
2019-08-21 20:31:33 -0400

    adjusted deployment

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -3,7 +3,7 @@ openssl aes-256-cbc -K $encrypted_532cd7f1946c_key -iv $encrypted_532cd7f1946c_i
 
 chmod 0400 $(pwd)/.deploy/travis_id_rsa
 
-ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey1.rkeplin.com 'sed -i "s/image: '"$DOCKER_REPO_SLUG"':[a-zA-Z0-9]*/image: '"$DOCKER_REPO_SLUG"':'"$TRAVIS_BUILD_NUMBER"'/g" /opt/stacks/bible.yml'
+ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey1.rkeplin.com 'sed -i "s/image: rkeplin\/bible-php-api:[a-zA-Z0-9]*/image: rkeplin\/bible-php-api:'"$TRAVIS_BUILD_NUMBER"'/g" /opt/stacks/bible.yml'
 ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/travis_id_rsa travis@jersey1.rkeplin.com 'docker stack deploy -c /opt/stacks/bible.yml bible'
 
 rm -rf $(pwd)/.deploy/travis_id_rsa
